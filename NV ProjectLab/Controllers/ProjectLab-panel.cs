82b77a2faa90df5de6873cbef73a17afc0fd80fc
AGK-1 <wiktorasonovic@outlook.com>
2026-09-13 22:38:05 +0400

    project st add gitingnore

---
+++ NV ProjectLab/Controllers/ProjectLab-panel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NV_ProjectLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectLab_panel : ControllerBase
+    {
+        [HttpGet("my")]
+        public string myFunc()
+        {
+            return ("qsqs");
+        }
+    }
+}
