8f216f415b10d0d82fbcf5d08757cb1848ebed97
AGK-1 <wiktorasonovic@outlook.com>
2026-09-14 20:51:17 +0400

    Modified Interface

--- NV ProjectLab/Interfaces/IProjectService.cs
+++ NV ProjectLab/Interfaces/IProjectService.cs
@@ -3,10 +3,14 @@
     string AddCustomer(CustomerEntity customer);
 
     CustomerEntity GetCustomerWithId(int id);
-    IEnumerable<CustomerEntity> GetCustomers();
 
+    CustomerEntity GetCustomerWithEmail(string email);
+    IEnumerable<CustomerEntity> GetAllWaitingCustomers();
+    IEnumerable<CustomerEntity> GetCustomers();
+    CustomerEntity GetNextCustomer();
     string ChangeCustomerStatus(int id, CustomerStatus status);
     string DeleteCustomer(int id);
+    string DeleteCustomerWithEmail(string email);
 
 }
 
